03fcf33e55efa52cb1ace7a6ff8437081dfee9f6
wendy9910 <wendy9910@gmail.com>
2021-02-05 22:17:03 +0800

    Update script

--- Rounded_Cube/Assets/Cube.cs
+++ Rounded_Cube/Assets/Cube.cs
@@ -29,43 +29,23 @@ public class Cube : MonoBehaviour
 
         int v = 0;
         
-        for (int y = 0; y <= ySize; y++)
+        for (int y = 0; y <= ySize; y++)///高
         {
-            for (int x = 0; x <= xSize; x++){
+            for (int x = 0; x <= xSize; x++) {
                 vertices[v++] = new Vector3(x, y, 0);
                 yield return wait;
             }
-            for (int z = 1; z <= zSize; z++){
-                for (int x = 1; x < xSize; x++){
-                    vertices[v++] = new Vector3(xSize, y, z);
-                    yield return wait; 
-                }
-            }
-            for (int x = xSize - 1; x >= 0; x--){
-                vertices[v++] = new Vector3(x,y,zSize);
+            for (int z = 1; z <= zSize; z++) {
+                vertices[v++] = new Vector3(xSize , y, z);
                 yield return wait;
             }
-            for (int z = zSize -1; z > 0; z--){
-                for (int x = 1; x < xSize; z++){
-                    vertices[v++] = new Vector3(0, y, z);
-                    yield return wait;
-                }
+            for (int x = xSize - 1; x >= 0; x--) {
+                vertices[v++] = new Vector3(xSize, y,zSize);
+                yield return wait;
             }
-           for (int z = 1; z < zSize; z++)//有問題
-            {
-                for (int x = 1; x < xSize; x++)
-                {
-                    vertices[v++] = new Vector3(x, ySize, z);
-                    yield return wait;
-                }
-            }
-            for (int z = 1; z < zSize; z++)
-            {
-                for (int x = 1; x < xSize; x++)
-                {
-                    vertices[v++] = new Vector3(x, 0, z);
-                    yield return wait;
-                }
+            for (int z = zSize - 1; z > 0; z--) {
+                vertices[v++] = new Vector3(0, y ,z);
+                yield return wait;
             }
         }
         
