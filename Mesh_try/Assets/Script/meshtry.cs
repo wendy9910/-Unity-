3cfa767b9d1096e8da3e1d88697d63ea8fd2c791
wendy9910 <wendy991017@gmail.com>
2021-02-12 15:44:01 +0800

    new project

---
+++ Mesh_try/Assets/Script/meshtry.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshtry : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+
+        for (int i=0;i<vertices.Length;i++) {
+            vertices[i] += normals[i] * Mathf.Sin(Time.time);
+        }
+        mesh.vertices = vertices;
+        
+    }
+}
