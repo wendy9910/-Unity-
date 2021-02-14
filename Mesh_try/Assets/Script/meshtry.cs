53ff34fca3d16b7b658491367d234fca20531583
wendy9910 <wendy991017@gmail.com>
2021-02-14 16:33:48 +0800

    try

--- Mesh_try/Assets/Script/meshtry.cs
+++ Mesh_try/Assets/Script/meshtry.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 using UnityEngine;
 
 public class meshtry : MonoBehaviour
@@ -21,16 +21,20 @@ public class meshtry : MonoBehaviour
         mesh.name = "P_Grid";
 
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        for (int i=0, y=0; y<=ySize; y++) {
-            for (int x = 0; x <= xSize; x++, i++) {
-                vertices[i] = new Vector3(x,y);
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
                 yield return wait;
             }
         }
-        mesh.vertices = vertices;
 
+        mesh.vertices = vertices;
+        
         int[] triangles = new int[xSize * ySize * 6];
-        for(int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
         {
             for (int x = 0; x < xSize; x++, vi++, ti+=6) {
                 triangles[ti] = vi;
@@ -38,8 +42,9 @@ public class meshtry : MonoBehaviour
                 triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
                 triangles[ti + 5] = vi + xSize + 2;
                 mesh.triangles = triangles;
+               // mesh.RecalculateNormals();
                 yield return wait;
-            } 
+            }
         }
 
     }
