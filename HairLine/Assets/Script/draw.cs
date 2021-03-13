94870b22e6f4bf83d063de5d906a552ebcf571c3
wendy9910 <wendy991017@gmail.com>
2021-03-13 21:47:30 +0800

    add Sphere on line

--- HairLine/Assets/Script/draw.cs
+++ HairLine/Assets/Script/draw.cs
@@ -6,17 +6,19 @@ public class draw : MonoBehaviour
 {
     private LineRenderer player;
     public List<Vector3> MousePointPos = new List<Vector3>();
-    public List<Vector3> PointPos = new List<Vector3>();
+    public List<Vector3> SpherePos = new List<Vector3>();
+    public List<GameObject> SphereGroup = new List<GameObject>(); 
     private Vector3 LastPos;
     private bool Down;
     Vector3 MousePos;
+    int number0=1, number1=0;
 
     // Start is called before the first frame update
     void Start()
     {
         player = gameObject.AddComponent<LineRenderer>();
         player.material = new Material(Shader.Find("Sprites/Default"));
-        player.SetColors(Color.blue, Color.green);
+        player.SetColors(Color.green, Color.gray);
         player.SetWidth(0.01f, 0.01f);
     }
 
@@ -36,18 +38,27 @@ public class draw : MonoBehaviour
             LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
             MousePointPos.Add(LastPos);
             Down = true;
+            if (number1 > number0) {
+                number0++;
+            }
+            number1++;
         }
         if (Down == true) {
 
             MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
             float dist = Vector3.Distance(LastPos,MousePos);
-            if (dist > 0.01f) {//更新座標
+            if (dist > 0.05f) {//更新座標
                 MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
                 MousePointPos.Add(MousePos);
                 LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
                 player.positionCount = MousePointPos.Count;
                 player.SetPositions(MousePointPos.ToArray());
+                GameObject sphere;   
+                SphereGroup.Add(sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere));
+                sphere.transform.localScale = new Vector3(0.03f, 0.03f, 0.03f);
+                sphere.transform.position = MousePos;
             }
+
             Debug.Log(dist);
 
         }
@@ -55,14 +66,23 @@ public class draw : MonoBehaviour
             Down = false;
             MousePointPos.Clear();
             player = null;
+
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (number1 > number0) {
+            for (int i = 0; i <= SphereGroup.Count - 1; i++)
+            {
+                Destroy(SphereGroup[i]);
+            }
+            number0++;
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Space)) 顯示座標
         {
             for (int i = 0; i <= MousePointPos.Count - 1; i++)
             {
                 Debug.Log(MousePointPos[i]);
             }
-        }
+        }*/
 
     }
 
