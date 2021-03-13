cd5ccd9c29ed502971bdb7fa68064f121211258d
wendy9910 <wendy991017@gmail.com>
2021-03-13 18:54:39 +0800

    can draw hair

--- HairLine/Assets/Script/draw.cs
+++ HairLine/Assets/Script/draw.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class draw : MonoBehaviour
 {
     private LineRenderer player;
+    public List<Vector3> MousePointPos = new List<Vector3>();
     public List<Vector3> PointPos = new List<Vector3>();
+    private Vector3 LastPos;
+    private bool Down;
     Vector3 MousePos;
 
     // Start is called before the first frame update
@@ -24,26 +27,46 @@ public class draw : MonoBehaviour
         if (Input.GetMouseButtonDown(0)){
 
             MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
-            PointPos.Add(MousePos);
+
             player.numCapVertices = 2;//端點圓度
             player.numCornerVertices = 2;//拐彎圓滑度
-            player.positionCount = PointPos.Count;
-            player.SetPositions(PointPos.ToArray());
-  
+
+            player.positionCount = MousePointPos.Count;
+            player.SetPositions(MousePointPos.ToArray());
+            LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
+            MousePointPos.Add(LastPos);
+            Down = true;
+        }
+        if (Down == true) {
+
+            MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
+            float dist = Vector3.Distance(LastPos,MousePos);
+            if (dist > 0.01f) {//更新座標
+                MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
+                MousePointPos.Add(MousePos);
+                LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
+                player.positionCount = MousePointPos.Count;
+                player.SetPositions(MousePointPos.ToArray());
+            }
+            Debug.Log(dist);
+
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            Down = false;
+            MousePointPos.Clear();
+            player = null;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i <= PointPos.Count-1; i++)
+            for (int i = 0; i <= MousePointPos.Count - 1; i++)
             {
-                Debug.Log(PointPos[i]);
+                Debug.Log(MousePointPos[i]);
             }
         }
-     
+
     }
 
-    private void OnMouseDrag()
-    {
-        
-    }
+
+   
 
 }
