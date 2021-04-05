e1d03a2876c4fbd65bc0acca49b9976083ecad4e
wendy9910 <wendy991017@gmail.com>
2021-04-05 22:00:06 +0800

    update

--- SpringSystem/Assets/Scripts/LineSpring.cs
+++ SpringSystem/Assets/Scripts/LineSpring.cs
@@ -6,11 +6,14 @@ public class LineSpring : MonoBehaviour
 {
     private LineRenderer player;
     public List<Vector3> MousePointPos = new List<Vector3>();
+    public List<Vector3> SpherePos = new List<Vector3>();
     public List<GameObject> SphereGroup = new List<GameObject>();
     private Vector3 MousePos, LastPos;
     GameObject sphere;
     private bool Down;
     public float mass1 = 1f;
+    int count = 0;
+    int v = 5;
 
 
     // Start is called before the first frame update
@@ -18,27 +21,16 @@ public class LineSpring : MonoBehaviour
     {
         player = gameObject.AddComponent<LineRenderer>();
         player.material = new Material(Shader.Find("Sprites/Default"));
-        player.SetColors(Color.gray, Color.gray);
+        player.SetColors(Color.green, Color.gray);
         player.SetWidth(0.1f, 0.1f);
-        
-
     }
 
     void Update()
     {
-        MouseGet();
-        
-        /*if (Down == false) {
-            spring();
-            drawer();
-        }*/        
-
-    }
-
-    public void MouseGet()
-    {
+        player = GetComponent<LineRenderer>();
         if (Input.GetMouseButtonDown(0))
         {
+
             Vector3 mousePos = Input.mousePosition;
 
             float x0 = (mousePos.x - Screen.width / 2) / (Screen.width / 2);
@@ -50,17 +42,24 @@ public class LineSpring : MonoBehaviour
             MousePos = new Vector3(x, y, 0.0f);
             MousePointPos.Add(MousePos);
 
+            player.numCapVertices = 2;//端點圓度
+            player.numCornerVertices = 2;//拐彎圓滑度
+
             LastPos = new Vector3(x, y, 0.0f);
+
+            player.positionCount = MousePointPos.Count;
+            player.SetPositions(MousePointPos.ToArray());
 
             SphereGroup.Add(sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere));
             sphere.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
             sphere.transform.position = MousePos;
+            SpherePos.Add(sphere.transform.position);
 
             Rigidbody RG = sphere.AddComponent<Rigidbody>();
             RG.isKinematic = true;
             RG.mass = mass1;
 
-            Down = true;
+            Down = true;  
         }
         if (Down == true)
         {
@@ -73,69 +72,68 @@ public class LineSpring : MonoBehaviour
 
             MousePos = new Vector3(x, y, 0.0f);
             float dist = Vector3.Distance(LastPos, MousePos);
-
             if (dist > 1f)
             {//更新座標
                 MousePos = new Vector3(x, y, 0.0f);
                 MousePointPos.Add(MousePos);
                 LastPos = new Vector3(x, y, 0.0f);
 
+                
+
                 SphereGroup.Add(sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere));
                 sphere.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
                 sphere.transform.position = MousePos;
+                SpherePos.Add(sphere.transform.position);
                 Rigidbody RG = sphere.AddComponent<Rigidbody>();
                 RG.isKinematic = true;
                 RG.mass = mass1;
             }
-
         }
+
         if (Input.GetMouseButtonUp(0))
         {
             Down = false;
+            if (Down == false)
+            {
+                Spring();
+                for (int i = 0; i < SphereGroup.Count; i++)
+                {
+                    SpherePos[i] = SphereGroup[i].transform.position;
+                }
+                player.positionCount = SpherePos.Count;
+                player.SetPositions(SpherePos.ToArray());
+
+            }
             //MousePointPos.Clear();
         }
-        if (Down == false)
+
+        void Spring()
         {
-            Drawer();
-            //if(SphereGroup != null)Spring();
-        }
-
-    }
-
-    void Drawer()
-    {
-        player.numCapVertices = 2;//端點圓度
-        player.numCornerVertices = 2;//拐彎圓滑度
-        player = GetComponent<LineRenderer>();
-        player.positionCount = MousePointPos.Count;
-        player.SetPositions(MousePointPos.ToArray());
-
-    }
-
-    void Spring()
-    {
-        
             Rigidbody fristRG = SphereGroup[0].GetComponent<Rigidbody>();
             fristRG.isKinematic = true;
             fristRG.mass = mass1;
+
+            for (int i = 0; i < SphereGroup.Count - 1; i++)
+            {
+
+                SpringJoint MainSpring = SphereGroup[i].AddComponent<SpringJoint>();
+                MainSpring.spring = 20.0f;
+                MainSpring.damper = 5.0f;
+
+                SphereGroup[i].transform.position = MousePointPos[i];
+                SphereGroup[i + 1].transform.position = MousePointPos[i + 1];
+              
+                Rigidbody otherRG = SphereGroup[i + 1].GetComponent<Rigidbody>();
+
+                otherRG.isKinematic = false;
+                otherRG.mass = mass1;
+                MainSpring.connectedBody = otherRG;
+
+            }
+        }
+        
         
 
-        for (int i = 0; i < SphereGroup.Count - 1; i++)
-        {
-            SpringJoint MainSpring = SphereGroup[i].AddComponent<SpringJoint>();
-            //MainSpring.maxDistance = 0.05f;
-            MainSpring.spring = 20.0f;
-            MainSpring.damper = 5.0f;
-
-            SphereGroup[i].transform.position = MousePointPos[i];
-            SphereGroup[i + 1].transform.position = MousePointPos[i + 1];
-
-            Rigidbody otherRG = SphereGroup[i + 1].GetComponent<Rigidbody>();
-
-            otherRG.isKinematic = false;
-            otherRG.mass = mass1;
-            MainSpring.connectedBody = otherRG;
-        }
 
     }
 }
