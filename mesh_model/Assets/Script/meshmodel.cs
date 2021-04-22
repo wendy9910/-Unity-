d510d36f94eacdf92edcbab30d56692d058c29ce
wendy9910 <wendy991017@gmail.com>
2021-04-22 22:43:15 +0800

    update

--- mesh_model/Assets/Script/meshmodel.cs
+++ mesh_model/Assets/Script/meshmodel.cs
@@ -46,54 +46,17 @@ public class meshmodel : MonoBehaviour
 
 
         }
-        if (Input.GetMouseButtonUp(0))
-        {
-            down = 2;
-        }
+        if (Input.GetMouseButtonUp(0))down = 2;
+
         if (down == 2) {
 
             GetComponent<MeshFilter>().mesh = mesh = new Mesh();
             mesh.name = "Hair Grid";
 
-            //if(MousePointPos != null) vertices = new Vector3[(MousePointPos.Count / 2 - 1) * 6];
-
-            //vertices = new Vector3[MousePointPos.Count* 6];
-            //mesh.vertices = MousePointPos.ToArray();
             mesh.vertices = MousePointPos.ToArray();
-            /*int[] triangles = new int[6];
-            triangles[0] = 0;
-            triangles[1] = 2;
-            triangles[2] = 1;
-            triangles[3] = 2;
-            triangles[4] = 3;
-            triangles[5] = 1;
-        
-            mesh.triangles = triangles;*/
 
-            /*
-            int [] triangles = new int[MousePointPos.Count*6];
-            for (int ti = 0, vi = 0, x = 1; x < triangles.Length; x++, ti += 6, vi++)
-            {
-                triangles[ti] = vi; 
-                triangles[ti + 1] = vi + 2; 
-                triangles[ti + 2] = vi + 1; 
-                triangles[ti + 3] = vi + 1;
-                triangles[ti + 4] = vi + 2;
-                triangles[ti + 5] = vi + 3;
-
-                mesh.triangles = triangles;
-
-                triangles[ti] = vi;
-                triangles[ti + 1] = vi + 1;
-                triangles[ti + 2] = vi + 2;
-                triangles[ti + 3] = vi + 2;
-                triangles[ti + 4] = vi + 1;
-                triangles[ti + 5] = vi + 3;
-            }
-            */
-            //int[] triangles = new int[(MousePointPos.Count / 2 -1) * 6];
-            int[] triangles = new int[MousePointPos.Count * 6];
-            for (int ti = 0, vi = 0, x = 0; x < triangles.Length; x++, ti += 6, vi+=2)
+            int[] triangles = new int[(MousePointPos.Count / 2 -1) * 6];
+            for (int ti = 0, vi = 0, x = 0; x < MousePointPos.Count / 2 - 1; x++, ti += 6, vi+=2)
             {
                 triangles[ti] = vi;
                 triangles[ti + 1] = vi + 1;
@@ -111,7 +74,7 @@ public class meshmodel : MonoBehaviour
     void Generate(Vector3 pos1,Vector3 pos2)
     {
         Vector3 Vec0 = pos1-pos2;
-        Vector3 Vec1 = new Vector3(Vec0.y,-Vec0.x,0);
+        Vector3 Vec1 = new Vector3(Vec0.y,-Vec0.x,10.0f);
         Vector3 AddPos = new Vector3(pos1.x+Vec1.x,pos1.y+Vec1.y,pos1.z);
         MousePointPos.Add(AddPos);    
     }
