94e961005b709ece7021fcb3fbe2607a1b700b9b
wendy9910 <wendy991017@gmail.com>
2021-05-03 16:44:17 +0800

    update

--- mesh_model/Assets/Script/meshmodel3.cs
+++ mesh_model/Assets/Script/meshmodel3.cs
@@ -8,7 +8,7 @@ public class meshmodel3 : MonoBehaviour
     public List<Vector3> MousePointPos = new List<Vector3>();
     private Vector3[] thickness1;
     private Vector3[] thickness2;
-    private Vector3[] vertices;
+ 
     private Vector3 MousePos, LastPos;
     private Mesh mesh;
     public int width = 1;
@@ -25,30 +25,30 @@ public class meshmodel3 : MonoBehaviour
     void Update()
     {
  
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space"))//設定mesh寬度
         {
             width++;
             Debug.Log("Range" + width);
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))//劃出髮片路徑抓座標
         {
             down = 1;
-            MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20.0f));//new position
             
-            LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20.0f));//last position
 
         }
         if (down == 1)
         {
-            MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20.0f));
 
-            float dist = Vector3.Distance(LastPos, MousePos);
+            float dist = Vector3.Distance(LastPos, MousePos);//座標間距
             if (dist > 1.0f)
             {
-                WidthGenerate(MousePos, LastPos);
-                MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-                LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+                WidthGenerate(MousePos, LastPos);//點座標計算函式
+                MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20.0f));
+                LastPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20.0f));
             }
         }
         if (Input.GetMouseButtonUp(0)) down = 2;
@@ -66,26 +66,26 @@ public class meshmodel3 : MonoBehaviour
         mesh.name = "Hair Grid";
 
         Vector3[] moscov = MousePointPos.ToArray();
-        Vector2[] uv = new Vector2[MousePointPos.Count];
+        Vector2[] uv = new Vector2[MousePointPos.Count];//texture
         Vector4[] tangents = new Vector4[MousePointPos.Count];
         Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
 
-        for (int i = 0; i < MousePointPos.Count; i++)
+        for (int i = 0; i < MousePointPos.Count; i++)//Vector3轉Vector2
         {
             uv[i].x = MousePointPos[i].x;
             uv[i].y = MousePointPos[i].y;
             tangents[i] = tangent;
         }
 
-        mesh.vertices = MousePointPos.ToArray();
+        mesh.vertices = MousePointPos.ToArray();//mesh網格點生成
         mesh.uv = uv;
         mesh.tangents = tangents;
         int point = 0;
          
-        point = ((MousePointPos.Count / (3 + (width-1)*2 ) - 1)) * 2 * width;
+        point = ((MousePointPos.Count / (3 + (width-1)*2 ) - 1)) * 2 * width;//計算網格數
         
 
-        int[] triangles = new int[point * 6];
+        int[] triangles = new int[point * 6];//計算需要多少三角形點座標
 
         int t = 0;//初始三角形
         int k = 0;//累加
@@ -112,11 +112,13 @@ public class meshmodel3 : MonoBehaviour
         return i + 6;
     }
 
-    void WidthGenerate(Vector3 pos1, Vector3 pos2)
+    void WidthGenerate(Vector3 pos1, Vector3 pos2)//計算點座標 (1)主線段點(2)右左兩個延伸點座標計算
     {
+        //右左兩個延伸點座標矩陣
         thickness1 = new Vector3[width];
         thickness2 = new Vector3[width];
-
+       
+        //算兩點向量差
         Vector3 Vec0 = pos1 - pos2;
         Vector3 pos = pos1;
 
@@ -134,7 +136,7 @@ public class meshmodel3 : MonoBehaviour
         }
     }
 
-    void WidthAdd1(Vector3 Vec, Vector3 pos)
+    void WidthAdd1(Vector3 Vec, Vector3 pos)//方便計算寬度改變
     {
         for (int i = 0;i < thickness1.Length;i++) {
             Vector3 Vec1 = new Vector3((Vec.y)*(i+1), (-Vec.x)*(i+1), 0.0f);
