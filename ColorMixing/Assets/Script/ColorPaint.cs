afe33d914c45ff6475c7470c285755f1dd0bdc18
wendy9910 <wendy991017@gmail.com>
2021-09-03 00:53:58 +0800

    劃出正弦波

--- ColorMixing/Assets/Script/ColorPaint.cs
+++ ColorMixing/Assets/Script/ColorPaint.cs
@@ -10,7 +10,7 @@ public class ColorPaint : MonoBehaviour
 
     int TexPixelLength = 256;
     Color[,] arrayColor;
-
+    public Texture Texture;
 
     // Start is called before the first frame update
     void Start()
@@ -19,28 +19,52 @@ public class ColorPaint : MonoBehaviour
         tex2D = new Texture2D(TexPixelLength,TexPixelLength,TextureFormat.RGBA32, true);
         //unity官網   public Texture2D(int width, int height, TextureFormat textureFormat = TextureFormat.RGBA32, bool mipChain = true, bool linear = false);
 
-
-
-    }
+ 
+        
+    }   
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<SpriteRenderer>().material.mainTexture = tex2D;
+        
         float i = 200;
         for (int y = 0; y < tex2D.height; y++)
         {
             for (int x = 0; x < tex2D.width; x++)
             {
                 //Color c = new Color(1f / 255, 100f / 255, 1f / 255);
-                arrayColor[x, y] = new Color(1f/255, 100f/255, 1f/255);
-                arrayColor[x, y] = new Color(100f / 255, 100f / 255, 100f / 255);
+    
+                //arrayColor[x, y] = new Color(100f / 255, 100f / 255, 100f / 255);
 
-                // Color color = ((x & y) != 0 ? new Color(255 / 255, 200 / 255, 20 / 255) : new Color(1f / 255, 100f / 255, 1f / 255));
-                tex2D.SetPixel(x, y, arrayColor[x,y]);
+                Color color = ((x & y) != 0 ? new Color(255 / 255, 200 / 255, 20 / 255) : new Color(1f / 255, 100f / 255, 1f / 255));
+                tex2D.SetPixel(x, y, color);
             }
         }
         tex2D.Apply();
         ri.texture = tex2D;
+
+        Debug.Log(ri.transform.position);
+        Vector3 p = ri.transform.position;
+
+ 
+        if (Input.GetMouseButtonDown(0)) 
+        {
+
+            Vector3 mos = Camera.main.ScreenToWorldPoint(new Vector3(-Input.mousePosition.x, -Input.mousePosition.y, -10));
+            Debug.Log(mos);
+            if (mos.x < 230 + 100 && mos.x > 230 - 100 && mos.y < 240 + 100 && mos.y > 240 - 100)
+            {
+                float x1 = (mos.x - p.x) / 256;
+                float y1 = (mos.y - p.y) / 256;
+
+                Color c = tex2D.GetPixel((int)x1, (int)y1);
+
+                Debug.Log(c);
+            }
+
+        }
+
+   
     }
 }
---
+++ mesh_model/Assets/Script/BasicModel/wave.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wave : MonoBehaviour
+{
+    public List<Vector3> PointPos = new List<Vector3>();
+    public List<Vector3> UpdatePointPos = new List<Vector3>();
+    Vector3 oldPos, newPos;
+    float length = 10f;
+    Vector3 NormaizelVec;
+    int down = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*float angle = Mathf.PI * 2f;
+        for (int i = -10; i < 10; i++)
+        {
+            float x = i , y = Mathf.Sin(angle);
+            Vector3 Position = new Vector3(x, y + 4f , 0);
+            PointPos.Add(Position);
+            angle += Mathf.PI * 0.2f;
+        }*/
+
+        /*for (float angle = -Mathf.PI*6; angle < Mathf.PI*6; angle+= 0.9f) 
+        {
+            float x = angle * 0.9f, y = Mathf.Sin(angle);
+            Vector3 Position = new Vector3(x, y + 1f , 0);
+            PointPos.Add(Position);
+        }*/
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            oldPos = newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30.0f));
+            PointPos.Add(oldPos);
+            down = 1;
+        }
+        if (down == 1)
+        {
+            newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30.0f));
+            float dist = Vector3.Distance(oldPos,newPos);
+            Debug.Log(dist);
+            
+            if (dist > length)
+            {
+                n = 0;
+                NormaizelVec = newPos - oldPos;
+                NormaizelVec = Vector3.Normalize(NormaizelVec);
+                Vector3 NewVec = new Vector3(NormaizelVec.x * length, NormaizelVec.y * length, NormaizelVec.z * length);
+                newPos = NewVec + oldPos;
+                PointPos.Add(newPos);
+
+                if (n == 0)
+                {
+                    Wave(NormaizelVec);
+                    n = 1;
+                }
+                oldPos = newPos;
+            }
+            
+        }
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            down = 0;
+        }
+    }
+
+    int n = 0;
+    public void Wave(Vector3 NVec) 
+    {
+        /*float angle = Mathf.PI * 0.2f;
+        Vector3 TempVec = PointPos[0];//0 1 2 3
+        if (n == 0)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                float x = i, y = Mathf.Sin(angle);
+                Vector3 Position = new Vector3(TempVec.x, TempVec.y + y, 0);
+                UpdatePointPos.Add(Position);
+                angle += Mathf.PI * 0.2f;
+                TempVec += NVec;
+            }
+            n = 1;
+        }*/
+        
+        Vector3 TempVec = PointPos[PointPos.Count - 2];
+        for (float angle = -Mathf.PI; angle < Mathf.PI; angle += 0.9f)
+        {
+            float x = angle * 0.9f, y = Mathf.Sin(angle);
+            Vector3 Position = new Vector3(TempVec.x, y + TempVec.y, 0);
+            UpdatePointPos.Add(Position);
+            TempVec += (NVec*length)/7;
+        }
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        for (int i = 0; i < UpdatePointPos.Count; i++)
+        {
+            Gizmos.DrawSphere(UpdatePointPos[i], 0.5f);
+        }
+    }
+}
